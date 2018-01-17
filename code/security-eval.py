4d4b8a79d7eff8084bd9eadf81f776195eaaedec
Maisy Wilcox <milowilcox@hanovernorwichschools.org>
2018-01-17 09:24:44 -0500

    Update security-eval.py

--- code/security-eval.py
+++ code/security-eval.py
@@ -20,9 +20,32 @@ angBb=300
 angBb=270
 
 camAa=d
-camAb=
-camAc=
+camAb=[0,0]
+camAc=[0,0]
 
 camBa=b
-camBb=
-cambc=
+camBb=[0,0]
+cambc=[0,0]
+
+#finds the equations and limits for the moving walls
+
+
+#finds the intersection for camera views
+
+
+#finds the coverage if the camera view is on the same wall/wall plane
+
+
+#finds the coverage is the camera view falls upon more than one wall plane
+
+
+#checks that none of the camera side views lie on the same plane as a wall, and if they do, then includes that coverage
+
+
+#sums the coverage of all three sides of the camera
+
+
+#sums the coverage of both cameras
+
+
+#divides sumative coverage by all surface on which paintings can be hung and multiplies by 100 to give percent
