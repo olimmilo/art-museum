fa756a35ff7dc63b478d8cb121a648de24f900f6
Maisy Wilcox <milowilcox@hanovernorwichschools.org>
2018-01-23 01:12:25 -0500

    Update security-eval.py

--- code/security-eval.py
+++ code/security-eval.py
@@ -61,64 +61,64 @@ camBb1=[(math.cos(math.radians(angBb))*0.5),(math.sin(math.radians(angBb))*0.5)]
 
 #finds where the two lines intersect and if it falls within the first line
 def line(a1, b1, m1, a2, m2):
-  int=[0,0]
-  int[0]=((m1*a1[0])-a1[1]-(m2*a2[0])+a2[1])/(m1-m2)
-  int[1]=(m1*(int[0]-a1[0]))+a1[1]
+  sec=[0,0]
+  sec[0]=((m1*a1[0])-a1[1]-(m2*a2[0])+a2[1])/(m1-m2)
+  sec[1]=(m1*(int[0]-a1[0]))+a1[1]
   if a1[0] >= b1[0]:
-    if (b1[0] >= int[0]) and (int[0] >= a1[0]):
+    if (b1[0] >= sec[0]) and (sec[0] >= a1[0]):
       t=1
     else:
       t=0
   elif a1[0] <= b1[0]:
-    if (a1[0] >= int[0]) and (int[0] >= b1[0]):
+    if (a1[0] >= sec[0]) and (sec[0] >= b1[0]):
       t=1
     else:
       t=0
   if t == 1:
     if a1[1] >= b1[1]:
-      if (b1[1] >= int[1]) and (int[1] >= a1[1]):
+      if (b1[1] >= sec[1]) and (sec[1] >= a1[1]):
         t=1
       else:
         t=0
     elif a1[1] <= b1[1]:
-      if (a1[1] >= int[1]) and (int[1] >= b1[1]):
+      if (a1[1] >= sec[1]) and (sec[1] >= b1[1]):
         t=1
       else:
         t=0
-  return int,t
+  return sec,t
 
 #finds where the two lines (one verticle) intersect, and if it fals within the first one
 def vertline(a1, b1, a2, b2):
   if (a1[0]-b1[0]) == 0:
-    int=[a1[0],0]
+    sec=[a1[0],0]
     m=(a2[1]-b2[1])/(a2[0]-b2[0])
-    int[1]=(m*(a1[0]-a2[0]))+a2[1]
+    sec[1]=(m*(a1[0]-a2[0]))+a2[1]
   else:
-    int=[a2[0],0]
+    sec=[a2[0],0]
     m=(a1[1]-b1[1])/(a1[0]-b1[0])
-    int[1]=(m*(a2[0]-a1[0]))+a1[1]
+    sec[1]=(m*(a2[0]-a1[0]))+a1[1]
   if a1[0] >= b1[0]:
-    if (b1[0] >= int[0]) and (int[0] >= a1[0]):
+    if (b1[0] >= sec[0]) and (sec[0] >= a1[0]):
       t=1
     else:
       t=0
   elif a1[0] <= b1[0]:
-    if (a1[0] >= int[0]) and (int[0] >= b1[0]):
+    if (a1[0] >= sec[0]) and (sec[0] >= b1[0]):
       t=1
     else:
       t=0
   if t == 1:
     if a1[1] >= b1[1]:
-      if (b1[1] >= int[1]) and (int[1] >= a1[1]):
+      if (b1[1] >= sec[1]) and (sec[1] >= a1[1]):
         t=1
       else:
         t=0
     elif a1[1] <= b1[1]:
-      if (a1[1] >= int[1]) and (int[1] >= b1[1]):
+      if (a1[1] >= sec[1]) and (sec[1] >= b1[1]):
         t=1
       else:
         t=0
-  return int,t
+  return sec,t
 #finds the equations and limits for the moving walls
 if (AWa[0]-AWb[0]) == 0:
   UNDEFINEDA=1
