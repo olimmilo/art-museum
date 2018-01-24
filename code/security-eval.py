08feeea9e116373384b1da18dbdd5667c7e6daf0
Maisy Wilcox <milowilcox@hanovernorwichschools.org>
2018-01-24 02:11:15 -0500

    Update security-eval.py

--- code/security-eval.py
+++ code/security-eval.py
@@ -217,19 +217,19 @@ else:
 #image manipulation
 
 #walls and doors
-draw.line((AWa[0]*100, height-(AWa[1]*100), AWb[0]*100, height-(AWb[1]*100)), width=10, fill="green")
-draw.line((BWa[0]*100, height-(BWa[1]*100), BWb[0]*100, height-(BWb[1]*100)), width=10, fill="green")
-draw.line((CWa[0]*100, height-(CWa[1]*100), CWb[0]*100, height-(CWb[1]*100)), width=10, fill="green")
-draw.line((DWa[0]*100, height-(DWa[1]*100), DWb[0]*100, height-(DWb[1]*100)), width=10, fill="green")
-draw.line((EWa[0]*100, height-(EWa[1]*100), EWb[0]*100, height-(EWb[1]*100)), width=10, fill="green")
+draw.line((AWa[0]*100, height-(AWa[1]*100), AWb[0]*100, height-(AWb[1]*100)), width=10, fill="purple")
+draw.line((BWa[0]*100, height-(BWa[1]*100), BWb[0]*100, height-(BWb[1]*100)), width=10, fill="purple")
+draw.line((CWa[0]*100, height-(CWa[1]*100), CWb[0]*100, height-(CWb[1]*100)), width=10, fill="purple")
+draw.line((DWa[0]*100, height-(DWa[1]*100), DWb[0]*100, height-(DWb[1]*100)), width=10, fill="purple")
+draw.line((EWa[0]*100, height-(EWa[1]*100), EWb[0]*100, height-(EWb[1]*100)), width=10, fill="purple")
 
 draw.line((a[0]*100, a[1]*100, b[0]*100, b[1]*100), width=10, fill="black")
 draw.line((b[0]*100, b[1]*100, c[0]*100, c[1]*100), width=10, fill="black")
 draw.line((c[0]*100, c[1]*100, d[0]*100, d[1]*100), width=10, fill="black")
 draw.line((d[0]*100, d[1]*100, a[0]*100, a[1]*100), width=10, fill="black")
 
-draw.line((a[0]*100, a[1]*100, (a[0]+2)*100, a[1]*100), width=10, fill="black")
-draw.line((c[0]*100, c[1]*100, (c[0]+2)*100, c[1]*100), width=10, fill="black")
+draw.line((a[0]*100, a[1]*100, (a[0]+2)*100, a[1]*100), width=10, fill="green")
+draw.line((c[0]*100, c[1]*100, (c[0]+2)*100, c[1]*100), width=10, fill="green")
 
 
 
