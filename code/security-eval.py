06dc564011ac7e9f386bb93c6d0dad40cba48644
Maisy Wilcox <milowilcox@hanovernorwichschools.org>
2018-01-24 04:21:10 -0500

    Update security-eval.py

--- code/security-eval.py
+++ code/security-eval.py
@@ -303,6 +303,7 @@ draw.line((d[0]*100, d[1]*100, a[0]*100, a[1]*100), width=20, fill="black")
 draw.line((a[0]*100, height-(a[1]*100), (a[0]+2)*100, height-(a[1]*100)), width=40, fill="green")
 draw.line((c[0]*100, height-(c[1]*100), (c[0]-2)*100, height-(c[1]*100)), width=40, fill="green")
 
+"""
 #CAMAa potentials
 if tWACAMAa == 1:
   draw.line((camAc[0]*100, height-(camAc[1]*100),secWACAMAa[0]*100, height-(secWACAMAa[1]*100)), width=10, fill="red")
@@ -367,4 +368,5 @@ if tWDCAMBb == 1:
 if tWECAMBb == 1:
   draw.line((camBc[0]*100, height-(camBc[1]*100),secWECAMBb[0]*100, height-(secWECAMBb[1]*100)), width=10, fill="red")
 #image saving and testing
+"""
 im.show()
