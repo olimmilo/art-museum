9ff54ff425d47ba7f2da7c93aee5ef5186a207e8
Maisy Wilcox <milowilcox@hanovernorwichschools.org>
2018-01-24 02:50:14 -0500

    Update security-eval.py

--- code/security-eval.py
+++ code/security-eval.py
@@ -218,9 +218,7 @@ else:
 
 #draw.line((camAc[0]*100, height-(camAc[1]*100)),(secWACAMAa[0]*100, height-(secWACAMAa[1]*100)), width=10, fill="purple")
 
-#draw.line((500,500),(0,0),width=10,fill="red")
-
-draw.line((500, 500), (0, 500), width=10, fill="purple")
+#draw.line((500,500, 0,0),width=10,fill="red")
 
 #walls, doors, and camera locations
 draw.line((AWa[0]*100, height-(AWa[1]*100), AWb[0]*100, height-(AWb[1]*100)), width=10, fill="purple")
